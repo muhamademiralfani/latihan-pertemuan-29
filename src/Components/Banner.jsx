a339b4a750ffb0668054ccbc4bc588156a7b4bff
Dede Supriatna <dedesupriatna1421@mail.com>
2024-11-13 20:57:37 +0700

    feat(components): add portfolio page

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -10,9 +10,12 @@ import Button from "./Button";
 */
 const Banner = ({ image, text, button }) => {
   return (
-    <div className={`relative w-full bg-cover bg-[url(${image})] p-3 rounded-md`}>
-      <div className="absolute inset-0 bg-primary/50 rounded-md"></div>
-      <div className="relative flex flex-col items-center justify-center h-full text-white min-h-28">
+    <div className={`relative w-full bg-cover overflow-hidden p-3 rounded-md`}>
+      <div>
+        <img className="absolute top-0 left-0 w-full" src={image} alt="banner image" />
+      </div>
+      <div className="absolute inset-0 bg-primary/80 rounded-md"></div>
+      <div className="relative flex flex-col items-center justify-center h-full text-white min-h-36">
 				<q className="text-white text-l font-semibold mb-2 text-center">
 					{text}
 				</q>
