7add477f295696219db03ab0f8ee7686122a4a72
muhamademiralfani <muhamademiralfani@gmail.com>
2024-11-13 15:30:26 +0700

    feat(Layouts): add Navbar Components and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Button from './Components/Button'
+import React from 'react';
+import Navbar from './Layouts/Navbar';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Services from './Pages/Services';
 
 const App = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <Button>Click me</Button>
-    </div>
-  )
-}
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/service' element={<Services />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
