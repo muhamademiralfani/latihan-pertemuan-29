23be5b8d51d642ee655cee20981821cefe08a1d5
Dede Supriatna <dedesupriatna1421@mail.com>
2024-11-13 19:24:00 +0700

    refactor: add mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Layouts/Navbar';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Portfolio from './Pages/Portfolio';
 import Services from './Pages/Services';
+import Blogs from './Pages/Blogs';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path='/contact' element={<Contact />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/service' element={<Services />} />
+          <Route path='/blog' element={<Blogs />} />
         </Routes>
       </Router>
     </>
