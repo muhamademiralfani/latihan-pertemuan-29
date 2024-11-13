f723d576de120e9a8418e720947abf8999c26bf4
muhamademiralfani <muhamademiralfani@gmail.com>
2024-11-13 21:41:51 +0700

    fix(Components): fix responsive layout component infoCard

--- src/Components/InfoCard.jsx
+++ src/Components/InfoCard.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import infoIcon from '../assets/img/Our Vision/Black-1.jpg';
 
-// Build Component InfoCard and export it to Component Page with props data
 const InfoCard = ({ bgImage, title, number = '', image, desc }) => {
   return (
-    <div className='border-2 w-[555px] h-[100px] p-4 flex items-center gap-4 rounded-lg'>
-      <div className={`w-[60px] h-[60px] rounded-md  ${bgImage} flex items-center justify-center`}>
-        <img src={image} alt='' />
+    <div className=' p-4 flex flex-col sm:flex-row items-center gap-4 rounded-lg w-full bg-white'>
+      <div className={`w-full sm:w-auto h-[60px] rounded-md ${bgImage} flex items-center justify-center`}>
+        <img src={image} alt='' className='max-w-full h-auto object-contain' />
       </div>
-      <div>
-        <h3 className='text-primary font-bold text-[20px]'>{title}</h3>
-        <p>
+      <div className='text-center'>
+        <h3 className='text-primary font-bold'>{title}</h3>
+        <p className='text-base'>
           {desc}
-          <span className='text-accent ml-3'>{number}</span>{' '}
+          {number && <span className='text-accent ml-3'>{number}</span>}
         </p>
       </div>
     </div>
